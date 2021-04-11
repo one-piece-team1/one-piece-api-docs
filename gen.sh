a07c2374af72fc488db3a9a93127e894b6e36ee6
lib <dreak603@gmail.com>
2021-04-11 17:41:57 +0800

    chore: moving folder and revise script

--- gen.sh
+++ gen.sh
@@ -2,10 +2,12 @@
 
 set -x
 
-echo "\033[34m[Generate] Generate API $1 Documents \033[0m"
-docgen build -i  $(pwd)/apis/API-$1.json -o $(pwd)/index.html
-if [[ "$?" == 1 ]]; then
-    echo "\033[31m\xE2\x9C\x96 Generate API $1 Documents fail, abort docs\033[0m"
-    exit 1
+if [[ "$1" == 'api' || "$1" == "API" ]]; then
+  echo "\033[34m[Generate] Generate API $2 Documents \033[0m"
+  docgen build -i  $(pwd)/apis/json/API-$2.json -o $(pwd)/apis/docs/index.html
+  if [[ "$?" == 1 ]]; then
+      echo "\033[31m\xE2\x9C\x96 Generate API $2 Documents fail, abort docs\033[0m"
+      exit 1
+  fi
+  echo "\033[32m\xE2\x9C\x94 Generate API $2 Documents Pass \033[0m"
 fi
-echo "\033[32m\xE2\x9C\x94 Generate API $1 Documents Pass \033[0m"
